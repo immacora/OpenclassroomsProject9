ab0f735ff9d1c1290194c49c269a268cdddd0057
Coralie <immacora@gmail.com>
2023-03-05 11:16:43 +0100

    Ajout du template posts

--- LITReview/reviews/urls.py
+++ LITReview/reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, PostsPageView
 
 
 urlpatterns = [
     path("home/", HomePageView.as_view(), name="home"),
+    path("posts/", PostsPageView.as_view(), name="posts"),
 ]
--- LITReview/reviews/views.py
+++ LITReview/reviews/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+class PostsPageView(TemplateView):
+    template_name = "posts.html"
