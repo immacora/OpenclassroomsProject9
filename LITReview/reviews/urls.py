63f4c3ec604cf7e651cf1cbb676679723c4f3de7
Coralie <immacora@gmail.com>
2023-03-07 11:36:55 +0100

    Modifications après 1er test de fonctionnement de la structure du projet

--- LITReview/reviews/urls.py
+++ LITReview/reviews/urls.py
@@ -3,6 +3,6 @@ from .views import HomePageView, PostsPageView
 
 
 urlpatterns = [
-    path("home/", HomePageView.as_view(), name="home"),
-    path("posts/", PostsPageView.as_view(), name="posts"),
+    path('', HomePageView.as_view(), name='home'),
+    path('posts/', PostsPageView.as_view(), name='posts'),
 ]
--- LITReview/reviews/views.py
+++ LITReview/reviews/views.py
@@ -1,9 +1,10 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, DetailView
 
 
 class HomePageView(TemplateView):
-    template_name = "home.html"
+    template_name = 'home.html'
 
 
 class PostsPageView(TemplateView):
-    template_name = "posts.html"
+    template_name = 'posts.html'
