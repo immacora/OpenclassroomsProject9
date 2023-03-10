fbc67db12f032c909da8f48481777fb0380c1034
Coralie <immacora@gmail.com>
2023-03-10 08:55:18 +0100

    Ajout affichage media et modification du nav register

--- LITReview/LITReview/urls.py
+++ LITReview/LITReview/urls.py
@@ -1,4 +1,6 @@
 """LITReview URL Configuration"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -13,4 +15,4 @@ urlpatterns = [
     # Local apps
     path('users/', include('users.urls')),
     path('', include("reviews.urls")),  
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
