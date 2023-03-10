904e90d97c38955f5ce1c1115eeb3783e8397d77
Coralie <immacora@gmail.com>
2023-03-11 00:47:08 +0100

    Ajout de la fonctionnalité de création

--- LITReview/reviews/urls.py
+++ LITReview/reviews/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import FeedsListView, PostsListView, TicketDetailView ,ReviewDetailView
+from .views import (
+    FeedsListView,
+    PostsListView,
+    TicketCreateView,
+    ReviewCreateView,
+    )
 
 
 urlpatterns = [
-    path('', FeedsListView.as_view(), name='home'),
+    path('review/new/', ReviewCreateView.as_view(), name='review_new'),
+    path('ticket/new/', TicketCreateView.as_view(), name='ticket_new'),
     path('posts/', PostsListView.as_view(), name='posts'),
-    path('posts/<uuid:pk>/', TicketDetailView.as_view(), name='ticket_detail'),
-    path('posts/<uuid:pk>/', ReviewDetailView.as_view(), name='review_detail'),
+    path('', FeedsListView.as_view(), name='home'),
 ]
--- LITReview/reviews/views.py
+++ LITReview/reviews/views.py
@@ -1,15 +1,33 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+
 from .models import Ticket, Review
 
 
-class FeedsPageView(LoginRequiredMixin, ListView):
+class FeedsListView(LoginRequiredMixin, ListView):
     model = Ticket
     context_object_name = 'ticket_list'
     template_name = 'home.html'
     login_url = 'login'
 
 
-class PostsPageView(LoginRequiredMixin, TemplateView):
+class PostsListView(LoginRequiredMixin, ListView):
+    model = Ticket
+    context_object_name = 'ticket_list'
     template_name = 'posts.html'
+    login_url = 'login'
+
+
+class TicketCreateView(LoginRequiredMixin, CreateView):
+    model = Ticket
+    template_name = 'ticket_new.html'
+    fields = ["title", "description", "image"]
+    login_url = 'login'
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    template_name = 'review_new.html'
+    fields = ["ticket", "rating", "headline", "body"]
     login_url = 'login'
