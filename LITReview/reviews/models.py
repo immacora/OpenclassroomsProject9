900a288abedfea8cbc509a6171c879db49b5bc9f
Coralie <immacora@gmail.com>
2023-03-10 21:53:04 +0100

    Mise à jour de securite des modèles Ticket et Review

--- LITReview/reviews/models.py
+++ LITReview/reviews/models.py
@@ -1,9 +1,16 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
+import uuid
 from django.db import models
+from django.urls import reverse
 
 
 class Ticket(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     title = models.CharField(max_length = 128)
     description = models.TextField(
         max_length = 2048,
@@ -22,9 +29,17 @@ class Ticket(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('ticket_detail', args=[str(self.id)])
 
 
 class Review(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     ticket = models.ForeignKey(
         to = Ticket,
         on_delete = models.CASCADE,
@@ -47,3 +62,6 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Critique du ticket : {self.ticket}"
+    
+    def get_absolute_url(self):
+        return reverse('review_detail', args=[str(self.id)])
--- LITReview/reviews/urls.py
+++ LITReview/reviews/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import FeedsPageView, PostsPageView
+from .views import FeedsListView, PostsListView, TicketDetailView ,ReviewDetailView
 
 
 urlpatterns = [
-    path('', FeedsPageView.as_view(), name='home'),
-    path('posts/', PostsPageView.as_view(), name='posts'),
+    path('', FeedsListView.as_view(), name='home'),
+    path('posts/', PostsListView.as_view(), name='posts'),
+    path('posts/<uuid:pk>/', TicketDetailView.as_view(), name='ticket_detail'),
+    path('posts/<uuid:pk>/', ReviewDetailView.as_view(), name='review_detail'),
 ]
