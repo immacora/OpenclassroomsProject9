edb12c2d921bfbfcd316463e8b9696d749c66c65
Coralie <immacora@gmail.com>
2023-03-14 15:30:55 +0100

    MAJ des styles

--- LITReview/users/admin.py
+++ LITReview/users/admin.py
@@ -11,7 +11,7 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['username',]
+    list_display = ['username', 'email', 'is_staff',]
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
