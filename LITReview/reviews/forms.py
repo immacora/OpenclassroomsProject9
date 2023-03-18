8542194c7adde8a06a18a5ece949e02218bf78f4
Coralie <immacora@gmail.com>
2023-03-18 19:47:35 +0100

    Reset

--- LITReview/reviews/forms.py
+++ LITReview/reviews/forms.py
@@ -1,16 +1,2 @@
 from django import forms
-from .models import Ticket, Review
 
-
-class TicketForm(forms.ModelForm):
-
-    class Meta:
-        model = Ticket
-        fields = ('title', 'description', 'image')
-
-
-class ReviewForm(forms.ModelForm):
-
-    class Meta:
-        model = Review
-        fields = ('headline', 'rating', 'body')
--- LITReview/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SignUpView, FollowsPageView
-
-
-urlpatterns = [
-    path("signup/", SignUpView.as_view(), name='signup'),
-    path("follows/", FollowsPageView.as_view(), name='follows'),
-]
