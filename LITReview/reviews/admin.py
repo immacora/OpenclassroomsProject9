9d359eff9395150ca4259696a38d41162512ed78
Coralie <immacora@gmail.com>
2023-03-19 20:29:40 +0100

    Ajout des admin

--- LITReview/reviews/admin.py
+++ LITReview/reviews/admin.py
@@ -1,1 +1,24 @@
 from django.contrib import admin
+from .models import Ticket, Review
+
+
+class ReviewInline(admin.StackedInline):
+    model = Review
+    extra = 0
+    fields = ('ticket', 'headline', 'rating', 'body', 'user', 'time_created')
+    #readonly_fields = ('time_created',)
+
+
+class TicketAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline,]
+    list_display = ('title', 'user', 'time_created', 'image', 'description')
+    #readonly_fields = ('time_created',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'user', 'time_created', 'headline', 'rating', 'body')
+    #readonly_fields = ('time_created',)
+
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
