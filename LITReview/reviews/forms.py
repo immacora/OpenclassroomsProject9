49e4efd81e52be9b39d362f2d46f9843a003f9a6
Coralie <immacora@gmail.com>
2023-03-22 17:29:01 +0100

    Ajout des formulaires ticket et review

--- LITReview/reviews/forms.py
+++ LITReview/reviews/forms.py
@@ -1,2 +1,16 @@
 from django import forms
+from .models import Ticket, Review
 
+
+class TicketForm(forms.ModelForm):
+
+    class Meta:
+        model = Ticket
+        fields = ['title', 'description', 'image',]
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ['headline', 'rating', 'body',]
