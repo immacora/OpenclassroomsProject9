926c0a3afb1f695ef368345366ee2e5471af1995
Coralie <immacora@gmail.com>
2023-03-26 20:13:19 +0200

    Ajout des modèles utilisateur

---
+++ LITReview/users/models.py
@@ -0,0 +1,46 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+import uuid
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    """Utilisateur personnalisé."""
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    followed_user = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        through='UserFollower'
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class UserFollower(models.Model):
+    """Abonnés."""
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='userfollower_user'
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='userfollower_followed_user'
+    )
+
+    class Meta:
+        unique_together=('user', 'followed_user')
+
+    def __str__(self):
+        return self.user.username
